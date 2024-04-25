fc31d21aecb6b938e369912094e6b29115cd9946
JhoanSLC <JhoanSLC4@gmail.com>
2024-04-25 15:32:41 -0500

    feat: :sparkles: Json files module functions
    All json files manipulatiopn functions created.

--- modules/files.py
+++ modules/files.py
@@ -1,8 +1,28 @@
+import modules.screen as scr 
+import os
 import json
-def json_inventario(inventario):
-    with open('data/inventario.json',"w") as ce:
-        json.dump(inventario,ce,indent=3)
 
-def add_book():
-    name_book= input(str("Enter the name of the book"))  
-    genre=input(str("book genre"))
+BASE = 'DATA/'
+
+# Checks if the json file exists and if it doesn't creates it
+# Receive: json file's name
+def checkFile(file:str):
+    if not os.path.isfile(BASE+file): # Checks if json file exists in DATA folder
+        with open(BASE+file, 'w') as createFile: 
+            json.dump({}, createFile, indent = 4) #Creates json file with an empty object
+
+    
+# Reads json file and loads it into a python dict
+# Receive: json file's name
+# Returns: Json file's content
+def readFile(file:str):
+    with open (BASE+file, 'r') as readFile: # Open json files and store it in a variable
+        return json.load(readFile)  
+    
+
+# Updates json file with the given content
+# Receive: json file's name
+#          content to update the file
+def updateFile(file:str, content):
+    with open(BASE+file, 'w+') as updateFile: # Open json files and store it in a variable
+        json.dump(content, updateFile, indent = 4)
